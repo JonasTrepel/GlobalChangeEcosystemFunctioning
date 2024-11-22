c6b7a993fc0cd19fadf91314cd553318e8575cea
Jonas Trepel <jonas.trepel@gmail.com>
2024-11-22 17:32:23 +0100

    start with RGEE

---
+++ R/prep/getTempDataRgee.R
@@ -0,0 +1,58 @@
+#install.packages("geojsonio")
+library(rgee)
+library(data.table)
+library(tidyverse)
+ee_Initialize(user = "jonas.trepel@bio.au.dk", drive = TRUE)
+
+
+years <- c(1950:2023)
+
+annual_temps <- data.table()
+
+for(year in years){
+  
+  print(year)
+  start <- paste0(year, "-01-01")
+  end <- paste0(year, "-12-31")
+  
+  annualComp <- ee$
+    #ImageCollection('ECMWF/ERA5_LAND/MONTHLY_AGGR')$
+    ImageCollection('ECMWF/ERA5_LAND/MONTHLY_AGGR')$
+    select('temperature_2m')$
+    filterDate(start, end)$
+    mean()$subtract(273.15)
+
+  
+  world_ext <- ee$Geometry$Rectangle(
+    coords = c(-179.99999, -89.9999, 179.99999, 89.9999),
+    proj = "EPSG:4326",
+    geodesic = FALSE
+  )
+  
+  acR <- ee_as_rast(image = annualComp,
+                    region = world_ext,
+                    container = "RGEE",
+                    dsn = "annCompMat", 
+                   # scale = 500,
+                    timePrefix = FALSE,
+                    maxPixels = 1e13,
+                    via = "drive")
+ 
+  plot(acR)
+  
+  drive_rm("annCompMat.tif")
+  googledrive::drive_empty_trash()
+  
+  acDT <- as.data.frame(acR, xy = T)
+  names(acDT) <- c("x", "y",  paste0("temperature_2m_", year))
+  
+  if(year == min(years)){
+    annual_temps <- acDT
+  }else{
+    annual_temps <- left_join(acDT, annual_temps)
+  }
+
+}
+  
+fwrite(annual_temps, "data/ProcessedData/annual_mat_era5.csv")
+
---
+++ R/prep/sortRGee.R
@@ -0,0 +1,42 @@
+### try google earth engine R for the 100th time
+
+# install.packages(c("remotes", "googledrive"))
+# remotes::install_github("r-spatial/rgee")
+
+## may be we have to set the whole path to /opt/anaconda3/envs/rgee_env/bin?? 
+rgee_env_dir <- c("/opt/anaconda3/envs/rgee_env")
+
+library(rgee)
+
+reticulate::use_python(paste0(rgee_env_dir, "/bin"), required=T)
+rgee::ee_install_set_pyenv(
+  py_path = rgee_env_dir,
+  py_env = 'rgee_env'
+)
+
+#Set system environment variables
+Sys.setenv(RETICULATE_PYTHON = rgee_env_dir)
+Sys.setenv(EARTHENGINE_PYTHON = rgee_env_dir)
+
+
+
+ee$Authenticate(auth_mode='notebook')
+
+# Initialize - this will connect to a project. You should always call this
+# before working with rgee. It is IMPORTANT THAT YOU SPECIFY A PROJECT using
+# the project parameter. If you forget what project IDs you have access to, find them
+# here: console.cloud.google.com/project
+ee$Initialize(project='ee-jonastrepel')  # <-- EDIT THIS FOR YOUR PROJECT
+
+# Optionally make a request to verify you are connected.
+ee$String('Hello from the Earth Engine servers!')$getInfo()
+
+# Create visualization parameters
+viz_params <- list(min = 0, max = 1000)
+
+# Add the layer to the map
+Map$centerObject(elev, 6)  # Centers the map on the elevation data
+Map$addLayer(elev, list(min = 0, max = 7000))
+
+
+
