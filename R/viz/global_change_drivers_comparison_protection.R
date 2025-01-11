a0df4e52972be684c6d87b859f7400eaa93296b9
Jonas Trepel <jonas.trepel@gmail.com>
2025-01-11 16:26:12 +0100

    add driver comp. pa

--- R/viz/global_change_drivers_comparison_protection.R
+++ R/viz/global_change_drivers_comparison_protection.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(data.table)
 library(remotePARTS)
 library(tidylog)
-
+library(scico)
 #load trends 
 
 evi_trends <- fread("data/processedData/dataFragments/pa_evi_trends.csv")
@@ -11,12 +11,10 @@ greenup_trends <- fread("data/processedData/dataFragments/pa_greenup_trends.csv"
 
 
 dt <- fread("data/processedData/data_with_response_timeseries/pas_and_controls_with_climate_trends.csv") %>% 
-  group_by(FunctionalBiome) %>% 
+  group_by(functional_biome) %>% 
   mutate(n_per_functional_biome = n()) %>% 
   ungroup() %>% 
   as.data.frame() %>% 
-  # filter(n_per_functional_biome > 1000) %>% 
-  rename(functional_biome = FunctionalBiome) %>% 
   mutate(
     productivity = case_when(
       grepl("L", functional_biome) ~ "low", 
@@ -35,17 +33,18 @@ dt <- fread("data/processedData/data_with_response_timeseries/pas_and_controls_w
       !grepl("C", functional_biome) & !grepl("B", functional_biome) & grepl("T", functional_biome) ~ "not_cold_tall", 
       !grepl("C", functional_biome) & !grepl("B", functional_biome) & grepl("S", functional_biome) ~ "not_cold_short"
     ),
-    nitrogen_depo = NitrogenDepo,
+    nitrogen_depo = nitrogen_depo,
     mat_coef = mat_coef,
     max_temp_coef = max_temp_coef,
     map_coef = map_coef,
-    human_modification = HumanModification, 
+    human_modification = human_modification, 
     area_km2_log = log(area_km2 + 0.0001),
-    pa_age_log = log(PaAge + 0.0001)) %>%
-  rename(pa_age = PaAge) %>% 
+    pa_age_log = log(pa_age + 0.0001)) %>%
   left_join(evi_trends) %>% 
   left_join(burned_area_trends) %>% 
   left_join(greenup_trends)
+
+dt$protection_cat_broad
 
 p_comp <- dt %>% 
   pivot_longer(cols = c(human_modification, nitrogen_depo, mat_coef, max_temp_coef, map_coef), 
@@ -58,8 +57,8 @@ p_comp <- dt %>%
       global_change_driver == "max_temp_coef" ~ "Max Temp Trend", 
       global_change_driver == "map_coef" ~ "MAP Trend"), 
     clean_prot = case_when(
-      og_layer == "controls" ~ "Control", 
-      og_layer == "protected_areas" ~ "Protected")) %>% 
+      protection_cat_broad == "control" ~ "Control", 
+      protection_cat_broad == "strictly_protected" ~ "Protected")) %>% 
   ggplot() +
   geom_hline(yintercept = 0, linetype = "dashed", color = "grey25") +
   geom_jitter(aes(x = clean_prot, y = global_change_value), alpha = 0.25, size = 0.5, color = "grey50") +
@@ -74,7 +73,7 @@ ggsave(plot = p_comp, "builds/plots/global_change_drivers_comparison_protection.
 #### calculate differences 
 
 dt_pa <- dt %>% 
-  filter(og_layer == "protected_areas")
+  filter(protection_cat_broad == "strictly_protected")
 
 dt_diff <- data.table()
 
@@ -85,6 +84,12 @@ for(pa_id in unique(dt_pa$unique_id)){
   tmp_diff$evi_diff <- dt[dt$unique_id %in% pa_id, ]$evi_coef - dt[dt$control_for %in% pa_id, ]$evi_coef
   tmp_diff$burned_area_diff <- dt[dt$unique_id %in% pa_id, ]$burned_area_coef - dt[dt$control_for %in% pa_id, ]$burned_area_coef
   tmp_diff$greenup_diff <- dt[dt$unique_id %in% pa_id, ]$greenup_coef - dt[dt$control_for %in% pa_id, ]$greenup_coef
+  tmp_diff$human_modification_diff <- dt[dt$unique_id %in% pa_id, ]$human_modification - dt[dt$control_for %in% pa_id, ]$human_modification
+  tmp_diff$nitrogen_depo_diff <- dt[dt$unique_id %in% pa_id, ]$nitrogen_depo - dt[dt$control_for %in% pa_id, ]$nitrogen_depo
+  tmp_diff$mat_diff <- dt[dt$unique_id %in% pa_id, ]$mat_coef - dt[dt$control_for %in% pa_id, ]$mat_coef
+  tmp_diff$max_temp_diff <- dt[dt$unique_id %in% pa_id, ]$max_temp_coef - dt[dt$control_for %in% pa_id, ]$max_temp_coef
+  tmp_diff$map_diff <- dt[dt$unique_id %in% pa_id, ]$map_coef - dt[dt$control_for %in% pa_id, ]$map_coef
+  
   tmp_diff$unique_id <- pa_id
   tmp_diff$functional_biome <- dt[dt$unique_id %in% pa_id, ]$functional_biome
   tmp_diff$super_biome <- dt[dt$unique_id %in% pa_id, ]$super_biome
@@ -99,29 +104,40 @@ for(pa_id in unique(dt_pa$unique_id)){
   print(paste0(pa_id, " done"))
 }
 
-
-dt_diff %>%
+p_diff <- dt_diff %>%
   mutate(clean_biome = case_when(
     super_biome == "cold_short" ~ "Cold Limited\nShort Vegetation",
     super_biome == "cold_tall" ~ "Cold Limited\nTall Vegetation",
     super_biome == "not_cold_short" ~ "Not Cold Limited\nShort Vegetation",
     super_biome == "not_cold_tall" ~ "Not Cold Limited\nTall Vegetation"
   )) %>% 
-  pivot_longer(cols = c("evi_diff", "greenup_diff", "burned_area_diff"), 
+  pivot_longer(cols = c("evi_diff", "greenup_diff", "burned_area_diff",
+                        "human_modification_diff", "nitrogen_depo_diff", 
+                        "mat_diff", "map_diff", "max_temp_diff"), 
                names_to = "diff_name", values_to = "diff_value") %>% 
   mutate(clean_diff = case_when(
     diff_name == "evi_diff" ~ "EVI\nTrend Diff.", 
     diff_name == "greenup_diff" ~ "Greenup\nTrend Diff.", 
-    diff_name == "burned_area_diff" ~ "Burned Area\nTrend Diff."
+    diff_name == "burned_area_diff" ~ "Burned Area\nTrend Diff.",
+    diff_name == "human_modification_diff" ~ "Human Modification\nDiff.", 
+    diff_name == "nitrogen_depo_diff" ~ "Nitrogen Depo\n Diff.", 
+    diff_name == "mat_diff" ~ "MAT\nTrend Diff.",
+    diff_name == "max_temp_diff" ~ "Max Temp\nTrend Diff.", 
+    diff_name == "map_diff" ~ "MAP\nTrend Diff."
   )) %>% 
   ggplot() +
   geom_hline(yintercept = 0, linetype = "dashed", color = "grey25") +
-  geom_jitter(aes(x = clean_biome, y = diff_value), alpha = 0.25, size = 0.5, color = "grey50", width = 0.2) +
+  geom_jitter(aes(x = clean_biome, y = diff_value, color = clean_biome), alpha = 0.25, size = 0.5, width = 0.2) +
   geom_boxplot(aes(x = clean_biome, y = diff_value), outlier.size = 0.25, outlier.shape = NA, alpha = 0.75) + 
-  facet_wrap(~clean_diff, scales = "free") +
+  scale_color_scico_d("batlowK") +
+  facet_wrap(~clean_diff, scales = "free", ncol = 4) +
   theme_bw() +
-  labs(x = "", y = "") +
-  theme(axis.text.x = element_text(angle = 70, hjust = 1))
+  labs(x = "", y = "", subtitle = "positive values indicate variable in PA > variable in control") +
+  theme(axis.text.x = element_text(angle = 70, hjust = 1), 
+        legend.position = "none")
+p_diff
+
+ggsave(plot = p_diff, "builds/plots/driver_diff_pas.png", dpi = 600)
 
 
 summary(lm(evi_diff ~ 1, data = dt_diff)); AIC(lm(evi_diff ~ 1, data = dt_diff))#66521.04
